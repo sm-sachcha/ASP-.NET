375fe4f7555dd8eeed256895ae5aaacd93953c39
sm-sachcha <sachcha.aiub@gmail.com>
2023-12-03 15:07:18 +0600

    Add files via upload

---
+++ NewsPortal/BLL/DTO/NewsDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewsPortal.Models
+{
+    public class NewsDTO
+    {
+        public int id { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
